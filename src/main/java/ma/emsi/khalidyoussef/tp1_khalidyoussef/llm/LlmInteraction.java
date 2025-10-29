cf990165006b4bcb0f831ccfe52ac92b2b10943e
Ykhali <Youssef.Khalid@emsi-edu.ma>
2025-10-29 10:18:33 +0100

    commit 5: ajout des classes LlmInteraction, JsonUtilPourGemini, LlmClientPourGemini et RequeteException

---
+++ src/main/java/ma/emsi/khalidyoussef/tp1_khalidyoussef/llm/LlmInteraction.java
@@ -0,0 +1,8 @@
+package ma.emsi.khalidyoussef.tp1_khalidyoussef.llm;
+
+public record LlmInteraction(
+        String questionExtraite,
+        String questionJson,
+        String reponseJson
+) {
+}
